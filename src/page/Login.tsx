d21f85011b7135ad9d675f108582fbe1276ec4ec
yashoku <70477548+yashoku233@users.noreply.github.com>
2024-02-06 18:47:01 +0800

    dev:修改登录和注册页面 加入表单

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,18 +1,76 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { REGISTER_PATHNAME } from '../router'
+
+import styles from './Register.module.scss'
+import { get } from 'http'
+
+const { Title } = Typography
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORA_KEY = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORA_KEY, password)
+}
+
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORA_KEY)
+}
+
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORA_KEY),
+  }
+}
 
 const Login: FC = () => {
-  const nav = useNavigate()
+  const [form] = Form.useForm() // 第三方hook
+
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+
+  const onFinish = (values: any) => {
+    const { username, password, remember } = values || {}
+    remember ? rememberUser(username, password) : deleteUserFromStorage()
+  }
+
   return (
-    <div>
-      <p>登录</p>
-      <button
-        onClick={() => {
-          nav(-1)
-        }}
-      >
-        返回上一层
-      </button>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} onFinish={onFinish} form={form}>
+        <Form.Item label="用户名" name="username">
+          <Input />
+        </Form.Item>
+        <Form.Item label="密码" name="password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item label="密码" name="remember" valuePropName="checked">
+          <Checkbox>记住我</Checkbox>
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+          <Space>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+            <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+          </Space>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
